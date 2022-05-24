2c7f9c9f5dfd7851c7c4969cf98a426093cfa4d4
Nihan Zyan <nihanzyan38@gmail.com>
2022-05-24 22:02:59 +0600

    Computer Parts Data Added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,34 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.raleo.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    console.log("database connected");
+    await client.connect();
+    const partCollection = client.db("pc-house").collection("parts");
+
+    app.get("/parts", async (req, res) => {
+      const query = {};
+      const cursor = partCollection.find(query);
+      const parts = await cursor.toArray();
+
+      res.send(parts);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+// root
 app.get("/", (req, res) => {
   res.send("Hello PC House");
 });
