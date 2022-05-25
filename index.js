4a8131daba2a8fea58737e314539ea71ecfaf00a
Nihan Zyan <nihanzyan38@gmail.com>
2022-05-25 13:55:33 +0600

    Single Data Api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -23,12 +23,20 @@ async function run() {
     await client.connect();
     const partCollection = client.db("pc-house").collection("parts");
 
-    app.get("/parts", async (req, res) => {
+    // LOAD MULTIPLE DATA
+    app.get("/part", async (req, res) => {
       const query = {};
       const cursor = partCollection.find(query);
       const parts = await cursor.toArray();
-
       res.send(parts);
+    });
+
+    // LOAD SINGLE DATA
+    app.get("/part/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const part = await partCollection.findOne(query);
+      res.send(part);
     });
   } finally {
   }
